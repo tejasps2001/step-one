cb6cd74bc477260bfd655ca194c98f5293713f4b
Tejas P S <tejasps2001@gmail.com>
2025-09-23 12:48:58 +0530

    Add getter method for location in Linear Movement

--- the-one/src/movement/LinearMovement.java
+++ the-one/src/movement/LinearMovement.java
@@ -14,39 +14,50 @@ import core.Settings;
 public class LinearMovement extends MovementModel {
 	/** Name space of the settings (append to group name space) */
 	public static final String LINEAR_MOVEMENT_NS = "LinearMovement.";
-	/** Per node group setting for defining the start coordinates of
-	 * the line ({@value}) */
+	/**
+	 * Per node group setting for defining the start coordinates of
+	 * the line ({@value})
+	 */
 	public static final String START_LOCATION_S = "startLocation";
-	/** Per node group setting for defining the end coordinates of
-	 * the line ({@value}) */
+	/**
+	 * Per node group setting for defining the end coordinates of
+	 * the line ({@value})
+	 */
 	public static final String END_LOCATION_S = "endLocation";
 
 	/**
 	 * Nodes' initial location type
-	 * <ul><li>0: random (evenly distributed)
+	 * <ul>
+	 * <li>0: random (evenly distributed)
 	 * <li>1: evenly spaced
 	 * <li>2: static location at the start of line
 	 * <li>3: evenly spaced among subsegment (needs startSegmentEndLocation)
 	 * </ul>
-	  */
+	 */
 	public static final String INIT_LOC_SEGMENT_END = "initLocSegmentEnd";
 	public static final String INIT_LOC_S = "initLocType";
 	/**
 	 * Nodes' target (where they're heading) type
-	 * <ul><li>0: random point on the line
+	 * <ul>
+	 * <li>0: random point on the line
 	 * <li>1: far-end of the line
 	 * </ul>
-	  */
+	 */
 	public static final String TARGET_S = "targetType";
-	private Coord initLocSegmentEnd; /** Used for node initial location distribution among segment*/
+	private Coord initLocSegmentEnd;
+	/** Used for node initial location distribution among segment */
 
 	/* values for the prototype */
-	private Coord startLoc; /** The start location of the line */
-	private Coord endLoc; /** The start location of the line */
+	private Coord startLoc;
+	/** The start location of the line */
+	private Coord endLoc;
+	/** The start location of the line */
 	private int initLocType;
 	private int targetType;
-	private int nodeCount; /** how many nodes in this formation */
-	private int lastIndex; /** index of the previous node */
+	private int nodeCount;
+	/** how many nodes in this formation */
+	private int lastIndex;
+	/** index of the previous node */
 
 	/* values for the per-node models */
 	private Path nextPath;
@@ -54,6 +65,7 @@ public class LinearMovement extends MovementModel {
 
 	/**
 	 * Creates a new movement model based on a Settings object's settings.
+	 * 
 	 * @param s The Settings object where the settings are read from
 	 */
 	public LinearMovement(Settings s) {
@@ -64,7 +76,7 @@ public class LinearMovement extends MovementModel {
 		this.startLoc = new Coord(coords[0], coords[1]);
 		coords = s.getCsvInts(LINEAR_MOVEMENT_NS + END_LOCATION_S, 2);
 		this.endLoc = new Coord(coords[0], coords[1]);
-		if (s.contains(LINEAR_MOVEMENT_NS + INIT_LOC_SEGMENT_END)){
+		if (s.contains(LINEAR_MOVEMENT_NS + INIT_LOC_SEGMENT_END)) {
 			coords = s.getCsvInts(LINEAR_MOVEMENT_NS + INIT_LOC_SEGMENT_END);
 			this.initLocSegmentEnd = new Coord(coords[0], coords[1]);
 		}
@@ -77,6 +89,7 @@ public class LinearMovement extends MovementModel {
 
 	/**
 	 * Copy constructor.
+	 * 
 	 * @param ilm The LinearFormation prototype
 	 */
 	public LinearMovement(LinearMovement ilm) {
@@ -99,16 +112,18 @@ public class LinearMovement extends MovementModel {
 		ilm.lastIndex++;
 	}
 
-	/** Spreads locations out evenly among the segment defined by initLocSegmentEnd (initLocType 3)*/
+	/**
+	 * Spreads locations out evenly among the segment defined by initLocSegmentEnd
+	 * (initLocType 3)
+	 */
 	private Coord calculateLocationForSegment(LinearMovement proto) {
 		double dx = 0;
 		double dy = 0;
 		double placementFraction;
 
-		double xDiff = (proto.initLocSegmentEnd.getX() -  proto.startLoc.getX());
-		double yDiff = (proto.initLocSegmentEnd.getY() -  proto.startLoc.getY());
+		double xDiff = (proto.initLocSegmentEnd.getX() - proto.startLoc.getX());
+		double yDiff = (proto.initLocSegmentEnd.getY() - proto.startLoc.getY());
 		Coord c = proto.startLoc.clone();
-
 
 		placementFraction = (1.0 * proto.lastIndex / proto.nodeCount);
 		dx = placementFraction * xDiff;
@@ -120,7 +135,8 @@ public class LinearMovement extends MovementModel {
 
 	/**
 	 * Calculates and returns a location in the line
-	 * @param proto The movement model prototype
+	 * 
+	 * @param proto  The movement model prototype
 	 * @param isEven Is the distribution evenly spaced (false for random)
 	 * @return a location on the line
 	 */
@@ -129,8 +145,8 @@ public class LinearMovement extends MovementModel {
 		double dy = 0;
 		double placementFraction;
 
-		double xDiff = (proto.endLoc.getX() -  proto.startLoc.getX());
-		double yDiff = (proto.endLoc.getY() -  proto.startLoc.getY());
+		double xDiff = (proto.endLoc.getX() - proto.startLoc.getX());
+		double yDiff = (proto.endLoc.getY() - proto.startLoc.getY());
 		Coord c = proto.startLoc.clone();
 
 		if (isEven) {
@@ -148,17 +164,18 @@ public class LinearMovement extends MovementModel {
 
 	/**
 	 * Calculates and returns the far-end of the line
-	 * @param proto The movement model prototype
+	 * 
+	 * @param proto   The movement model prototype
 	 * @param initLoc The initial location
 	 * @return the coordinates for the far-end of the line
 	 */
 	private Coord calculateEndTarget(LinearMovement proto, Coord initLoc) {
-		return (proto.startLoc.distance(initLoc) >
-			proto.endLoc.distance(initLoc) ? proto.startLoc: proto.endLoc);
+		return (proto.startLoc.distance(initLoc) > proto.endLoc.distance(initLoc) ? proto.startLoc : proto.endLoc);
 	}
 
 	/**
 	 * Returns the the location of the node in the formation
+	 * 
 	 * @return the the location of the node in the formation
 	 */
 	@Override
@@ -168,6 +185,7 @@ public class LinearMovement extends MovementModel {
 
 	/**
 	 * Returns a single coordinate path (using the only possible coordinate)
+	 * 
 	 * @return a single coordinate path
 	 */
 	@Override
@@ -183,7 +201,7 @@ public class LinearMovement extends MovementModel {
 	@Override
 	public double nextPathAvailable() {
 		if (nextPath == null) {
-			return Double.MAX_VALUE;	// no new paths available
+			return Double.MAX_VALUE; // no new paths available
 		} else {
 			return 0;
 		}
@@ -191,20 +209,26 @@ public class LinearMovement extends MovementModel {
 
 	@Override
 	public int getMaxX() {
-		return (int)(endLoc.getX() > startLoc.getX() ? endLoc.getX() :
-			startLoc.getX());
+		return (int) (endLoc.getX() > startLoc.getX() ? endLoc.getX() : startLoc.getX());
 	}
 
 	@Override
 	public int getMaxY() {
-		return (int)(endLoc.getY() > startLoc.getY() ? endLoc.getY() :
-			startLoc.getY());
+		return (int) (endLoc.getY() > startLoc.getY() ? endLoc.getY() : startLoc.getY());
 	}
-
 
 	@Override
 	public LinearMovement replicate() {
 		return new LinearMovement(this);
 	}
 
+	/**
+	 * Sets the initial and end location of the node.
+	 * @param initalLoc
+	 * @param finalLoc
+	 */
+	public void setLocation(Coord initalLoc, Coord finalLoc) {
+		this.initLoc = initalLoc;
+		this.endLoc = finalLoc;
+	}
 }
