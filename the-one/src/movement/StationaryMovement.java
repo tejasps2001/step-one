2cc986b9eb10ddd5a135826f020739270860a296
Tejas P S <tejasps2001@gmail.com>
2026-03-31 01:48:29 +0530

    Implemented the basic version of multi-rca

--- the-one/src/movement/StationaryMovement.java
+++ the-one/src/movement/StationaryMovement.java
@@ -4,6 +4,9 @@
  */
 package movement;
 
+import java.util.ArrayList;
+import java.util.List;
+import core.SettingsError;
 import core.Coord;
 import core.Settings;
 
@@ -15,6 +18,10 @@ public class StationaryMovement extends MovementModel {
 	/** Per node group setting for setting the location ({@value}) */
 	public static final String LOCATION_S = "nodeLocation";
 	private Coord loc; /** The location of the nodes */
+	
+	// Support for assigning multiple locations to a group of stationary nodes
+	private List<Coord> locations;
+	private static int nextHostIndex_sm = 0;
 
 	/**
 	 * Creates a new movement model based on a Settings object's settings.
@@ -22,10 +29,25 @@ public class StationaryMovement extends MovementModel {
 	 */
 	public StationaryMovement(Settings s) {
 		super(s);
-		double coords[];
-
-		coords = s.getCsvDoubles(LOCATION_S, 2);
-		this.loc = new Coord(coords[0],coords[1]);
+		String raw = s.getSetting(LOCATION_S);
+		if (raw.contains("[")) {
+			this.locations = new ArrayList<>();
+			raw = raw.replace("[", "").replace("]", "");
+			String[] pairs = raw.split(";");
+			for (String pair : pairs) {
+				String[] values = pair.split(",");
+				if (values.length != 2) {
+					throw new SettingsError("Invalid coordinate pair: " + pair + " in setting " + LOCATION_S);
+				}
+				locations.add(new Coord(Double.parseDouble(values[0].trim()), Double.parseDouble(values[1].trim())));
+			}
+			nextHostIndex_sm = 0;
+			this.loc = locations.get(0); // Default for prototype
+		} else {
+			double coords[] = s.getCsvDoubles(LOCATION_S, 2);
+			this.loc = new Coord(coords[0],coords[1]);
+			this.locations = null;
+		}
 	}
 
 	/**
@@ -35,6 +57,11 @@ public class StationaryMovement extends MovementModel {
 	public StationaryMovement(StationaryMovement sm) {
 		super(sm);
 		this.loc = sm.loc;
+		if (sm.locations != null && nextHostIndex_sm < sm.locations.size()) {
+			this.loc = sm.locations.get(nextHostIndex_sm++);
+		} else {
+			this.loc = sm.loc;
+		}
 	}
 
 	/**
